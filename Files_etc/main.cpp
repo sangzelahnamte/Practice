fd1458335f96113cbfa57e1489ec6090256d73d0
sangzelahnamte <sangaguitarist@gmail.com>
2025-02-16 00:37:14 -0800

    minor adjustment to the save method

--- Files_etc/main.cpp
+++ Files_etc/main.cpp
@@ -10,6 +10,7 @@ int main(int argc, char *argv[])
     QCoreApplication a(argc, argv);
 
     QString file_name = "lesson.txt";
+    save_file(file_name);
     load_file(file_name);
 
     return a.exec();
@@ -18,14 +19,21 @@ int main(int argc, char *argv[])
 void save_file(QString file_name) // write to file
 {
     QFile file(file_name);
-    if(!file.open(QIODevice::WriteOnly))
+    if(!file.open(QIODevice::WriteOnly)) // if it cannot open
     {
         qInfo() << file.errorString();
+        file.close();
+        return;
     }
-    QTextStream stream(&file);
-    stream.seek(file.size()); // always seek to the end of text stream to append the next string of text
-    stream << "text file saved";
-    stream.flush();
+    else
+    {
+        QTextStream stream(&file);
+        stream.seek(file.size()); // always seek to the end of text stream to append the next string of text
+        QString text = "some text that gets added\n";
+        stream << text;
+        file.write(text.toUtf8());
+        stream.flush();
+    }
     file.close();
 }
 
@@ -35,6 +43,7 @@ void load_file(QString file_name) // load to file
     if(!file.open(QIODevice::ReadOnly))
     {
         qInfo() << file.errorString();
+        file.close();
         return;
     }
     else
